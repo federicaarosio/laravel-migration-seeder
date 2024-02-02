788e353edab14aeb419d2a5ce263c012aaa180ef
federicaarosio <arosio.fede@gmail.com>
2024-02-02 17:38:20 +0100

    added filter (only trains with departure from today)

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index() {
-        $trains = Train::all();
-        // $trains = Train::where('departure_time' > '2024-02-02 00:00:00');
+        // $trains = Train::all();
+        $trains = Train::where('departure_time', '>=', date('2024-02-02').' 00:00:00')->get();
         return view('guest.train.index', ['trains' => $trains]);
     }
 }
