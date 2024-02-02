a33cc15f1c6d7289a511c61f7a30c993ce1efc22
federicaarosio <arosio.fede@gmail.com>
2024-02-02 17:10:34 +0100

    start working on style

--- resources/views/guest/train/index.blade.php
+++ resources/views/guest/train/index.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
 
 @forelse ($trains as $train)
-    Train from: {{ $train-> departure_station}} - to: {{ $train->arrival_station }}<br>
+    <strong>Train from:</strong> {{ $train-> departure_station}} - <strong>to:</strong> {{ $train->arrival_station }}<br>
 @empty
     <p>
         No trains here.
